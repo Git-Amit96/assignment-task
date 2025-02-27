059bfb31103086e451b00a93af4c2564ec8df7b5
Git-Amit96 <amitsaini962561@gmail.com>
2025-02-27 23:08:17 +0530

    Bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import UserDataForm from "./components/UserFormComponent"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserInfo from "./components/UserInfo";
+import { Suspense, lazy } from "react";
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
-import Auth from "./components/Auth";
 
-const App=() =>{
-return (
-  <Router>
-    <Navbar/>
-  <Routes>
-    <Route path="/" element={<Dashboard/>}/>
-    <Route path="/form" element={<UserDataForm />} />
-    <Route path="/user/:id" element={<UserInfo />} />
-    <Route path="/auth" element={<Auth />} />
-  </Routes>
-</Router>
-)
-  
-}
+// Lazy-loaded components
+const UserDataForm = lazy(() => import("./components/UserFormComponent"));
+const UserInfo = lazy(() => import("./components/UserInfo"));
+const Auth = lazy(() => import("./components/Auth"));
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Suspense fallback={<div className="text-center mt-10">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/form" element={<UserDataForm />} />
+          <Route path="/user/:id" element={<UserInfo />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
 
 export default App;
+
